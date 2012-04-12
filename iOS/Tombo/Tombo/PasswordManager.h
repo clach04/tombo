e3b2310ec5f7cfd656fe656871f26f46e87cad38
Hirami <tomohisa.hirami@nifty.com>
2012-04-12 22:05:07 +0900

    Support password timeout.

--- iOS/Tombo/Tombo/PasswordManager.h
+++ iOS/Tombo/Tombo/PasswordManager.h
@@ -4,6 +4,8 @@
 
 @property (strong,nonatomic) NSString *password;
 
+- (id)init;
+
 /*
  * If current password is not set (including timeout), popup dialog and ask to user. Or not, do nothing.
  * Note user may be cancel and finally prepare may be failed.
@@ -11,5 +13,9 @@
  */
 - (BOOL)preparePassword;
 
+/*
+ * Like preparePassword, but this method show password dialog twice. Second input is used to confirm
+ * the password is correct.
+ */
 - (BOOL)preparePasswordConfirm;
 @end
--- iOS/Tombo/Tombo/PasswordManager.m
+++ iOS/Tombo/Tombo/PasswordManager.m
@@ -1,35 +1,68 @@
 #import "PasswordManager.h"
 #import "SinglePasswordDialog.h"
 
-@implementation PasswordManager
+@implementation PasswordManager {
+    NSTimer *timer;
+}
 
 @synthesize password = _password;
 
+- (id)init {
+    id obj = [super init];
+    self.password = nil;
+    return obj;
+}
+
 - (BOOL)preparePassword {
-    SinglePasswordDialog *dialog = [[SinglePasswordDialog alloc] initWithTitle:@"Password" 
-                                                                       message:@"Please input password"];
-    NSString *pass = [dialog showAndWait];
-    if (pass == nil) return NO;
-    
-    self.password = pass;
+    if (!self.password) {
+        SinglePasswordDialog *dialog = [[SinglePasswordDialog alloc] initWithTitle:@"Password" 
+                                                                           message:@"Please input password"];
+        NSString *pass = [dialog showAndWait];
+        if (pass == nil) return NO;
+        
+        self.password = pass;
+    }
+
+    [self resetTimer];
     return YES;
 }
 
 - (BOOL)preparePasswordConfirm {
-    SinglePasswordDialog *dialog = [[SinglePasswordDialog alloc] initWithTitle:@"Password" 
-                                                                       message:@"Please input password"];
-    NSString *pass1 = [dialog showAndWait];
-    if (pass1 == nil) return NO;
-
-    dialog = [[SinglePasswordDialog alloc] initWithTitle:@"Confirm" message:@"Input password again"];
-    NSString *pass2 = [dialog showAndWait];
-    if (pass2 == nil) return NO;
-
-    if (![pass1 isEqualToString:pass2]) {
-        UIAlertView *mismatch = [[UIAlertView alloc] initWithTitle:@"Warn" message:@"Password mismatch." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
-        [mismatch show];
-        return NO;
+    if (!self.password) {
+        SinglePasswordDialog *dialog = [[SinglePasswordDialog alloc] initWithTitle:@"Password" 
+                                                                           message:@"Please input password"];
+        NSString *pass1 = [dialog showAndWait];
+        if (pass1 == nil) return NO;
+        
+        dialog = [[SinglePasswordDialog alloc] initWithTitle:@"Confirm" message:@"Input password again"];
+        NSString *pass2 = [dialog showAndWait];
+        if (pass2 == nil) return NO;
+        
+        if (![pass1 isEqualToString:pass2]) {
+            UIAlertView *mismatch = [[UIAlertView alloc] initWithTitle:@"Warn"
+                                                               message:@"Password mismatch."
+                                                              delegate:nil cancelButtonTitle:@"OK"
+                                                     otherButtonTitles:nil];
+            [mismatch show];
+            return NO;
+        }
     }
+
+    [self resetTimer];
+
     return YES;
+}
+
+- (void)resetTimer {
+    if (timer) [timer invalidate];
+    timer = [NSTimer scheduledTimerWithTimeInterval:60
+                                             target:self 
+                                           selector:@selector(fireTimer:) 
+                                           userInfo:nil 
+                                            repeats:NO];
+}
+
+- (void)fireTimer:(NSTimer *)timer {
+    self.password = nil;
 }
 @end
